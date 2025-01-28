1af15c603cb959f845a532eece1c8dadfb3ebc27
Tshering <12210038.gcit@rub.edu.bt>
2025-01-29 05:21:27 +0600

    upda

--- feedback-system-frontend/src/axios.js
+++ feedback-system-frontend/src/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://nppf-feedback-system.vercel.app/api' // Production backend URL
+    : 'http://localhost:5000/api'; // Local development backend URL
+
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Admin API base URL
+  baseURL: baseURL, // Dynamically set base URL
 });
 
 export default instance;
